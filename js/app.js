6432f29490170ebfa485ce6481ab5326bf389b3b
TimothyMoy <Timothy.z.moy@gmail.com>
2020-06-24 16:45:38 -0700

    added status counter

--- js/app.js
+++ js/app.js
@@ -30,8 +30,10 @@ let age = 0;
 const statusBar = 10;
 const pets = [];
 const status = document.getElementsByClassName('text');
-let hunger = document.getElementsByClassName('scale');
 const statArr = [0,1,2,3,4,5,6,7,8,9]
+let hungerIndex = 0
+let sleepIndex = 0
+let boredIndex = 0
 
 //event listeners
 //Start game button takes you to next screen
@@ -56,21 +58,32 @@ $('.characterButton').click(function(){
   //creates pets
   generatePets();
   //creates stat bar
-  generateStats();
+  // generateStats();
+  generateHunger();
+  generateSleep();
+  generateBored();
   //setName
   setName();
+  //TEST
+  hungerTimer();
+  sleepTimer();
+  boredTimer();
 })
 
 //Feed button test
 $('#feed').click(function(){
+  $( `.hungerStatus ${hungerIndex}` ).each(function() {
+    if (hungerIndex < 10){
+      let hungerNum = document.getElementById(`hunger${hungerIndex}`)
+        hungerNum.classList.remove('test2');
+        hungerNum.classList.add('test1');
+    }
+  });
+});
 
-  document.querySelector(".hungerStatus li .scale").innerHTML = "Clicked Index " + $(this).index();
 
-  $('.hungerStatus li .scale').eq(0).css("background-color","red");
 
-    $('.hungerStatus li .scale').eq(2).css("background-color","red");
-  console.log('this works');
-})
+
 
 //functions
 
@@ -130,24 +143,106 @@ function generatePets(){
 }
 
 //status
-function generateStats(){
-  // console.log('this works');
-  for (let i = 0; i < status.length; i ++){
-    // console.log(status[i]);
-    if (status[i].innerText === "Hunger") {
-      $('.hunger').append($("<ul class='hungerStatus'></ul>"));
-      //separate cateogry
-      //take first div and add red by index
-    } else if (status[i].innerText === "Sleepiness"){
-        $('.sleepiness').append($("<ul class='sleepStatus'></ul>"));
-    } else {
-        $('.boredom').append($("<ul class='boredomStatus'></ul>"));
-    }
-  } createScale()
+function generateHunger(){
+  $('.hunger').append($("<ul class='hungerStatus'></ul>"));
+  createHungerScale()
 }
 
-function createScale(){
-  for (let j = 0; j < statArr.length; j++) {
-      $('ul').append($("<li><div class='scale'></div></li>"));
+function generateSleep(){
+  $('.sleepiness').append($("<ul class='sleepStatus'></ul>"));
+  createSleepScale()
+}
+
+function generateBored(){
+  $('.boredom').append($("<ul class='boredomStatus'></ul>"));
+  createBoredScale()
+}
+
+
+
+function createHungerScale(){
+  for (let a = 0; a < statArr.length; a++) {
+      statArr[a] = document.createElement('div');
+      statArr[a].id = "hunger" + a;
+      statArr[a].classList.add("test1");
+      $(".hungerStatus").append(statArr[a]);
   }
 }
+
+function createSleepScale(){
+  for (let b = 0; b < statArr.length; b++) {
+      statArr[b] = document.createElement('div');
+      statArr[b].id = "sleep" + b;
+      statArr[b].classList.add("test1");
+      $(".sleepStatus").append(statArr[b]);
+  }
+}
+
+function createBoredScale(){
+  for (let c = 0; c < statArr.length; c++) {
+      statArr[c] = document.createElement('div');
+      statArr[c].id = "bored" + c;
+      statArr[c].classList.add("test1");
+      $(".boredomStatus").append(statArr[c]);
+  }
+}
+
+function hungerTimer() {
+setInterval(function (){
+  if (hungerIndex < 10){
+    let hungerNum = document.getElementById(`hunger${hungerIndex}`)
+      // console.log(divNum);
+      hungerNum.classList.remove('test1');
+      hungerNum.classList.add('test2');
+      hungerIndex+=1;
+    } else {
+    clearInterval(hungerIndex);
+  }
+},1000)
+}
+
+function boredTimer() {
+setInterval(function (){
+  if (boredIndex < 10){
+    let boredNum = document.getElementById(`bored${boredIndex}`)
+      // console.log(divNum);
+      boredNum.classList.remove('test1');
+      boredNum.classList.add('test2');
+      boredIndex+=1;
+    } else {
+    clearInterval(boredIndex);
+  }
+},1000)
+}
+
+function sleepTimer() {
+setInterval(function (){
+  if (sleepIndex < 10){
+    let sleepNum = document.getElementById(`sleep${sleepIndex}`)
+      // console.log(divNum);
+      sleepNum.classList.remove('test1');
+      sleepNum.classList.add('test2');
+      sleepIndex+=1;
+    } else {
+    clearInterval(sleepIndex);
+  }
+},1000)
+}
+
+
+
+// function generateStats(){
+//   // console.log('this works');
+//   for (let i = 0; i < status.length; i ++){
+//     // console.log(status[i]);
+//     if (status[i].innerText === "Hunger") {
+//       $('.hunger').append($("<ul class='hungerStatus'></ul>"));
+//       //separate cateogry
+//       //take first div and add red by index
+//     } else if (status[i].innerText === "Sleepiness"){
+//         $('.sleepiness').append($("<ul class='sleepStatus'></ul>"));
+//     } else {
+//         $('.boredom').append($("<ul class='boredomStatus'></ul>"));
+//     }
+//   } createScale()
+// }
